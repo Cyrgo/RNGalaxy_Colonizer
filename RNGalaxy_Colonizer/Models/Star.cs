a7b43f7ddf141eb0fb6ca2ee9fe7bb1de6e416bb
RamonVe <rvel01@protonmail.com>
2022-06-07 22:30:46 -0500

    Creates enum of SpectralTypes and implements it in class. Creates method to return string of star type based on spectral type.

--- RNGalaxy_Colonizer/Models/Star.cs
+++ RNGalaxy_Colonizer/Models/Star.cs
@@ -1,21 +1,60 @@
-﻿namespace RNGalaxy_Colonizer.Models; 
+﻿namespace RNGalaxy_Colonizer.Models;
+
+public enum SpectralType {
+    // Large Blue Super Giant
+    O,
+
+    // Medium Blue Super Giant
+    B,
+
+    // Small Blue Super Giant
+    A,
+
+    // Yellow Super Giant
+    F,
+
+    // Yellow Giant
+    G,
+
+    // Orange Giant
+    K,
+
+    // Red Giant
+    M,
+
+    // White Dwarf
+    D
+}
 
 public class Star {
-    public string SpectralType;
-    public int Temperature;
-    public double Radius;
-    public double Mass;
-    public double Luminosity;
-    public int LifeTime;
-    public double Abundance;
+    public SpectralType SpectralType { get; }
+    public int Temperature { get; }
+    public double Radius { get; }
+    public double Mass { get; }
+    public double Luminosity { get; }
+    public int LifeTime { get; }
 
-    public Star(string spectralType, int temperature, double radius, double mass, double luminosity, int lifeTime, double abundance) {
+    public Star(SpectralType spectralType, int temperature, double radius, double mass, double luminosity, int lifeTime) {
         this.SpectralType = spectralType;
         this.Temperature = temperature;
         this.Radius = radius;
         this.Mass = mass;
         this.Luminosity = luminosity;
         this.LifeTime = lifeTime;
-        this.Abundance = abundance;
+    }
+
+    public static string GetStarType(SpectralType spectralType) {
+        var starType = spectralType switch {
+            SpectralType.O => "Large Blue Super Giant",
+            SpectralType.B => "Medium Blue Super Giant",
+            SpectralType.A => "Small Blue Super Giant",
+            SpectralType.F => "Yellow Super Giant",
+            SpectralType.G => "Yellow Giant",
+            SpectralType.K => "Orange Giant",
+            SpectralType.M => "Red Giant",
+            SpectralType.D => "White Dwarf",
+            _ => "Yellow Giant"
+        };
+        return starType;
     }
 }
