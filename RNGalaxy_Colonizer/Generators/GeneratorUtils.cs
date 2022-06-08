77efb57df9fa07bdcf672fc8014b3aa6c54b1a02
RamonVe <rvel01@protonmail.com>
2022-06-07 22:29:03 -0500

    Creates util class to generate a random double in a range.

---
+++ RNGalaxy_Colonizer/Generators/GeneratorUtils.cs
@@ -0,0 +1,8 @@
+﻿namespace RNGalaxy_Colonizer.Generators;
+
+public static class GeneratorUtils {
+    public static double RandomDouble(double min, double max) {
+        var random = new Random();
+        return random.NextDouble() * (max - min) + min;
+    }
+}
