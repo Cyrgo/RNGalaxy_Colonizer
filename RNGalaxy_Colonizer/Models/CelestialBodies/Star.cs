4516338dee0c35c72c519f29a1abb351c81fc044
RamonVe <rvel01@protonmail.com>
2022-06-11 01:42:22 -0500

    -Moves console write line logic into Details method.

---
+++ RNGalaxy_Colonizer/Models/CelestialBodies/Star.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using RNGalaxy_Colonizer.Models.Enums;
+
+namespace RNGalaxy_Colonizer.Models.CelestialBodies;
+
+public class Star {
+    private SpectralType SpectralType { get; }
+    private string StarType => GetStarType(this.SpectralType);
+    private int Temperature { get; }
+    private double Radius { get; }
+    private double Mass { get; }
+    private double Luminosity { get; }
+    private int LifeTime { get; }
+
+    public Star(SpectralType spectralType, int temperature, double radius, double mass, double luminosity, int lifeTime) {
+        this.SpectralType = spectralType;
+        this.Temperature = temperature;
+        this.Radius = radius;
+        this.Mass = mass;
+        this.Luminosity = luminosity;
+        this.LifeTime = lifeTime;
+    }
+
+    private static string GetStarType(SpectralType spectralType) {
+        var starType = spectralType switch {
+            SpectralType.O => "Large Blue Super Giant",
+            SpectralType.B => "Medium Blue Super Giant",
+            SpectralType.A => "Small Blue Super Giant",
+            SpectralType.F => "Yellow Super Giant",
+            SpectralType.G => "Yellow Giant",
+            SpectralType.K => "Orange Giant",
+            SpectralType.M => "Red Giant",
+            SpectralType.D => "White Dwarf",
+            _ => "Red Giant"
+        };
+        return starType;
+    }
+
+    public void Details() {
+        Console.WriteLine("Spectral Type: " + this.SpectralType);
+        Console.WriteLine("Star Type: " + this.StarType);
+        Console.WriteLine("Temperature: " + this.Temperature + "K");
+        Console.WriteLine("Radius: " + this.Radius.ToString(CultureInfo.CurrentCulture) + " times Earth's Sun");
+        Console.WriteLine("Mass: " + this.Mass.ToString(CultureInfo.CurrentCulture) + " times Earth's Sun");
+        Console.WriteLine("Luminosity: " + this.Luminosity.ToString(CultureInfo.CurrentCulture) + " times Earth's Sun");
+        Console.WriteLine("Lifetime: " + this.LifeTime + " Million Years");
+    }
+}
