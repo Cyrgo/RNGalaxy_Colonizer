e8824fe902c1d856b7dd218ae301a6bef0d5e36c
RamonVe <rvel01@protonmail.com>
2022-06-11 01:43:42 -0500

    -Creates three planet type enum files.

---
+++ RNGalaxy_Colonizer/Models/Enums/PlanetTypes/GiantPlanetType.cs
@@ -0,0 +1,16 @@
+﻿namespace RNGalaxy_Colonizer.Models.Enums.PlanetTypes;
+
+public enum GiantPlanetType {
+    BrownDwarf,
+    ChtonianPlanet,
+    EccentricJupiter,
+    GasGiant,
+    GiantPlanet,
+    HeliumPlanet,
+    HotJupiter,
+    HotNeptune,
+    IceGiant,
+    MiniNeptune,
+    PuffyPlanet,
+    SuperJupiter
+}
